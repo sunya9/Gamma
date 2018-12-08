85692fdac2b61e9e7b3ad241c63c74b19c7eb3f6
sunya9 <mugen.xyz@gmail.com>
2018-12-09 07:16:11 +0900

    Add HomeFragment

---
+++ app/src/main/java/net/unsweets/gamma/fragment/HomeFragment.kt
@@ -0,0 +1,65 @@
+package net.unsweets.gamma.fragment
+
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.StringRes
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import net.unsweets.gamma.R
+import net.unsweets.gamma.databinding.FragmentHomeBinding
+
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_home, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.viewPager.adapter = StreamViewPagerAdapter(childFragmentManager, context)
+
+    }
+
+    class StreamViewPagerAdapter(fm: FragmentManager, val context: Context?) : FragmentPagerAdapter(fm) {
+        val items = listOf(
+            Item(PostItemFragment.newInstance(), R.string.home),
+            Item(PostItemFragment.newInstance(), R.string.mentions),
+            Item(PostItemFragment.newInstance(), R.string.interactions),
+            Item(PostItemFragment.newInstance(), R.string.stars)
+        )
+
+        data class Item(val fragment: Fragment, @StringRes val title: Int)
+
+        override fun getItem(position: Int): Fragment {
+            return items[position].fragment
+        }
+
+        override fun getCount(): Int {
+            return items.size
+        }
+
+        override fun getPageTitle(position: Int): CharSequence? {
+            return context?.getString(items[position].title)
+        }
+    }
+
+    companion object {
+        fun newInstance(): HomeFragment {
+            return HomeFragment()
+        }
+    }
+}
