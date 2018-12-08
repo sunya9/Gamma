1f2e14746b5997d0a3bcc65f59bf991e91187a54
sunya9 <mugen.xyz@gmail.com>
2018-12-09 07:25:47 +0900

    Add PostItemFragment

---
+++ app/src/main/java/net/unsweets/gamma/fragment/PostItemFragment.kt
@@ -0,0 +1,68 @@
+package net.unsweets.gamma.fragment
+
+import android.content.Context
+import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import net.unsweets.gamma.R
+import net.unsweets.gamma.adapter.PostItemRecyclerViewAdapter
+import net.unsweets.gamma.model.Post
+
+class PostItemFragment : Fragment() {
+
+    private var columnCount = 1
+
+    private var listener: OnListFragmentInteractionListener? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_postitem_list, container, false)
+
+        // Set the adapter
+        if (view is RecyclerView) {
+            with(view) {
+                layoutManager = when {
+                    columnCount <= 1 -> LinearLayoutManager(context)
+                    else -> GridLayoutManager(context, columnCount)
+                }
+                val items = ArrayList<Post>(0)
+//                adapter = PostItemRecyclerViewAdapter(this,items, listener)
+            }
+        }
+        return view
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is OnListFragmentInteractionListener) {
+            listener = context
+        } else {
+//            throw RuntimeException(context.toString() + " must implement OnListFragmentInteractionListener")
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
+    interface OnListFragmentInteractionListener {
+        fun onListFragmentInteraction(item: Post?)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() =
+            PostItemFragment().apply {
+                arguments = Bundle().apply {
+                }
+            }
+    }
+}
