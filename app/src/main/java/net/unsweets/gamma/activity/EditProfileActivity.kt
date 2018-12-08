3f5b42f2a9975cb49cd73e2fa54238a53d702cfb
sunya9 <mugen.xyz@gmail.com>
2018-12-09 06:10:10 +0900

    Add EditProfileActivity

---
+++ app/src/main/java/net/unsweets/gamma/activity/EditProfileActivity.kt
@@ -0,0 +1,13 @@
+package net.unsweets.gamma.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import net.unsweets.gamma.R
+
+class EditProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_profile)
+    }
+}
