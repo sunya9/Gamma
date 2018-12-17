aed34436ddb117364df1309d4fbc15d91e395595
sunya9 <mugen.xyz@gmail.com>
2018-12-18 07:56:42 +0900

    Add getMessage

--- app/src/main/java/net/unsweets/gamma/model/Interaction.kt
+++ app/src/main/java/net/unsweets/gamma/model/Interaction.kt
@@ -1,26 +1,32 @@
 package net.unsweets.gamma.model
 
+import android.content.Context
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import com.squareup.moshi.Json
+import net.unsweets.gamma.R
 import net.unsweets.gamma.model.raw.PollNotice
 import java.util.*
 
 sealed class Interaction(
-    @Json(name = "pagination_id")
     open val paginationId: String,
-    @Json(name = "event_date")
     open val eventDate: Date,
     open val action: Action,
     open val users: List<User>
 ) {
     data class Repost(
+        @Json(name = "pagination_id")
         override val paginationId: String,
+        @Json(name = "event_date")
         override val eventDate: Date,
         override val action: Action,
         override val users: List<User>,
         val objects: List<Post>
     ) : Interaction(paginationId, eventDate, action, users)
     data class Bookmark(
+        @Json(name = "pagination_id")
         override val paginationId: String,
+        @Json(name = "event_date")
         override val eventDate: Date,
         override val action: Action,
         override val users: List<User>,
@@ -28,7 +34,9 @@ sealed class Interaction(
     ) : Interaction(paginationId, eventDate, action, users)
 
     data class Reply(
+        @Json(name = "pagination_id")
         override val paginationId: String,
+        @Json(name = "event_date")
         override val eventDate: Date,
         override val action: Action,
         override val users: List<User>,
@@ -36,7 +44,9 @@ sealed class Interaction(
     ) : Interaction(paginationId, eventDate, action, users)
 
     data class Follow(
+        @Json(name = "pagination_id")
         override val paginationId: String,
+        @Json(name = "event_date")
         override val eventDate: Date,
         override val action: Action,
         override val users: List<User>,
@@ -44,24 +54,31 @@ sealed class Interaction(
     ) : Interaction(paginationId, eventDate, action, users)
 
     data class PollResponse(
+        @Json(name = "pagination_id")
         override val paginationId: String,
+        @Json(name = "event_date")
         override val eventDate: Date,
         override val action: Action,
         override val users: List<User>,
         val objects: List<PollNotice>
     ) : Interaction(paginationId, eventDate, action, users)
 
-    enum class Action {
+    enum class Action(@DrawableRes val iconRes: Int, @StringRes val actionRes: Int) {
         @Json(name = "repost")
-        Repost,
+        Repost(R.drawable.ic_cached_black_24dp, R.string.action_reposted_by),
         @Json(name = "bookmark")
-        Bookmark,
+        Bookmark(R.drawable.ic_star_black_24dp, R.string.action_starred_by),
         @Json(name = "reply")
-        Reply,
+        Reply(R.drawable.ic_reply_black_24dp, R.string.action_replied_by),
         @Json(name = "follow")
-        Follow,
+        Follow(R.drawable.ic_person_add_black_24dp, R.string.action_followed_by),
         @Json(name = "poll_response")
-        PollResponse
+        PollResponse(R.drawable.ic_poll_black_24dp, R.string.action_voted_by)
     }
 
+    fun getMessage(context: Context): CharSequence {
+        val actionStr = context.getString(action.actionRes)
+        val usersStr = users.joinToString(", ") { "@${it.username}" }
+        return context.getString(R.string.action_template_text, actionStr, usersStr)
+    }
 }
