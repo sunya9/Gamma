0a676d82a82d13d3a94d49eec4bb34f293ca160d
sunya9 <mugen.xyz@gmail.com>
2018-12-18 08:03:09 +0900

    fixup! Add utility classes for HaveEntities class

---
+++ app/src/main/java/net/unsweets/gamma/util/EntityOnTouchListener.kt
@@ -0,0 +1,21 @@
+package net.unsweets.gamma.util
+
+import android.text.Spannable
+import android.view.MotionEvent
+import android.view.View
+import android.widget.TextView
+
+// http://oigami.hatenablog.com/entry/2014/11/08/082615
+class EntityOnTouchListener: View.OnTouchListener {
+    private val method = LinkTouchMovementMethod()
+    override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+        if(v !is TextView || event == null) return false
+        v.apply {
+            movementMethod = method
+            val res = method.onTouchEvent(v, text as Spannable, event)
+            movementMethod = null
+            isFocusable = false
+            return res
+        }
+    }
+}
