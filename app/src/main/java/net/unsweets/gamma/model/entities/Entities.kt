8d08675788fb8375de0ee223382305d51b34ea74
sunya9 <mugen.xyz@gmail.com>
2018-12-18 02:07:15 +0900

    Use sealed class

--- app/src/main/java/net/unsweets/gamma/model/entities/Entities.kt
+++ app/src/main/java/net/unsweets/gamma/model/entities/Entities.kt
@@ -3,34 +3,36 @@ package net.unsweets.gamma.model.entities
 import com.squareup.moshi.Json
 
 data class Entities(
-    val links: List<LinkEntities>,
-    val mentions: List<MentionEntities>,
-    val tags: List<TagEntities>
+    val links: List<SealedEntity.LinkEntities>,
+    val mentions: List<SealedEntity.MentionEntities>,
+    val tags: List<SealedEntity.TagEntities>
 ) {
-    data class LinkEntities(
-        override val text: String,
-        override val len: Int,
-        override val pos: Int,
-        val link: String,
-        val title: String?,
-        val description: String?
-    ) : BaseEntities
+    sealed class SealedEntity : BaseEntities {
+        data class LinkEntities(
+            override val text: String,
+            override val len: Int,
+            override val pos: Int,
+            val link: String,
+            val title: String?,
+            val description: String?
+        ) : SealedEntity()
 
-    data class MentionEntities(
-        override val text: String,
-        override val len: Int,
-        override val pos: Int,
-        val id: String,
-        @Json(name = "is_leading") val isLeading: Boolean?,
-        @Json(name ="is_copy") val isCopy: Boolean?
-    ) : BaseEntities
+        data class MentionEntities(
+            override val text: String,
+            override val len: Int,
+            override val pos: Int,
+            val id: String,
+            @Json(name = "is_leading") val isLeading: Boolean?,
+            @Json(name = "is_copy") val isCopy: Boolean?
+        ) : SealedEntity()
 
 
-    data class TagEntities(
-        override val len: Int,
-        override val pos: Int,
-        override val text: String
-    ) : BaseEntities
+        data class TagEntities(
+            override val len: Int,
+            override val pos: Int,
+            override val text: String
+        ) : SealedEntity()
+    }
 
     interface BaseEntities {
         val len: Int
