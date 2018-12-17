dd8713c4ce9c862f9a4a189c447080c3116b49cf
sunya9 <mugen.xyz@gmail.com>
2018-12-18 02:10:27 +0900

    Add getSpannableStringBuilder

--- app/src/main/java/net/unsweets/gamma/model/entities/HaveEntities.kt
+++ app/src/main/java/net/unsweets/gamma/model/entities/HaveEntities.kt
@@ -1,7 +1,61 @@
 package net.unsweets.gamma.model.entities
 
+import android.content.Context
+import android.net.Uri
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.view.View
+import androidx.browser.customtabs.CustomTabsIntent
+import androidx.core.content.res.ResourcesCompat
+import net.unsweets.gamma.R
+import net.unsweets.gamma.fragment.ProfileFragment
+import net.unsweets.gamma.util.FragmentHelper
+import net.unsweets.gamma.util.TouchableSpan
+
 interface HaveEntities {
     val entities: Entities?
     val html: String?
     val text: String?
+    fun getSpannableStringBuilder(context: Context): SpannableStringBuilder {
+        val builder = SpannableStringBuilder(text)
+        val normalColor = ResourcesCompat.getColor(context.resources, R.color.colorPrimary, context.theme)
+        val pressedColor = ResourcesCompat.getColor(context.resources, R.color.colorPrimaryDarker, context.theme)
+        if (entities == null || text == null) return builder
+        entities?.let {
+            arrayListOf(it.links, it.mentions, it.tags)
+                .flatten()
+                .forEach { entity ->
+                    builder.setSpan(object : TouchableSpan(normalColor, pressedColor) {
+                        override fun onClick(widget: View) {
+                            clickedEntity(context, entity)
+                        }
+                    }, entity.pos, entity.pos + entity.len, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+                }
+        }
+        return builder
+    }
+
+    private fun clickedEntity(context: Context, entity: Entities.SealedEntity) {
+        when (entity) {
+            is Entities.SealedEntity.LinkEntities -> openLink(context, entity.link)
+            is Entities.SealedEntity.MentionEntities -> openProfile(context, entity.id)
+            is Entities.SealedEntity.TagEntities -> showTaggedPosts(context, entity.text)
+        }
+    }
+
+    companion object {
+        private fun showTaggedPosts(context: Context, tag: String) {
+            val fragment = ProfileFragment.newInstance(tag)
+            FragmentHelper.addFragment(context, fragment, tag)
+        }
+
+        private fun openProfile(context: Context, id: String) {
+            val fragment = ProfileFragment.newInstance(id)
+            FragmentHelper.addFragment(context, fragment, id)
+        }
+
+        private fun openLink(context: Context, url: String) {
+            CustomTabsIntent.Builder().build().launchUrl(context, Uri.parse(url))
+        }
+    }
 }
