e6f61ccf12f4bb6bd8135271f6c3752670482c34
sunya9 <mugen.xyz@gmail.com>
2019-03-08 06:51:54 +0900

    Add EditProfileActivity

--- app/src/main/java/net/unsweets/gamma/ui/activity/EditProfileActivity.kt
+++ app/src/main/java/net/unsweets/gamma/ui/activity/EditProfileActivity.kt
@@ -1,6 +1,8 @@
 package net.unsweets.gamma.ui.activity
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import net.unsweets.gamma.R
 
@@ -9,5 +11,31 @@ class EditProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
+        setTitle(R.string.edit_profile)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.edit_profile, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return item?.let {
+            when (it.itemId) {
+                android.R.id.home -> discard()
+                R.id.menuSave -> save()
+            }
+            true
+        } ?: super.onOptionsItemSelected(item)
+    }
+
+    private fun discard() {
+        // TODO: show confirmation dialog
+        finish()
+    }
+
+    private fun save() {
+        // TODO: show progress bar and save
+        finish()
     }
 }
