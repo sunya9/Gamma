5ba7c232fa7be9fc63bfc0565e5b522ec0bca59b
sunya9 <mugen.xyz@gmail.com>
2019-06-19 05:03:41 +0900

    Add private modifier

--- app/src/main/java/net/unsweets/gamma/domain/usecases/SetupTokenUseCase.kt
+++ app/src/main/java/net/unsweets/gamma/domain/usecases/SetupTokenUseCase.kt
@@ -5,7 +5,7 @@ import net.unsweets.gamma.domain.repository.IPnutRepository
 import net.unsweets.gamma.domain.repository.IPreferenceRepository
 import net.unsweets.gamma.domain.repository.PnutRepository
 
-class SetupTokenUseCase(val pnutRepository: IPnutRepository, val preferenceRepository: IPreferenceRepository) :
+class SetupTokenUseCase(val pnutRepository: IPnutRepository, private val preferenceRepository: IPreferenceRepository) :
     AsyncUseCase<SetupTokenOutputData, Unit>() {
     override suspend fun run(params: Unit): SetupTokenOutputData {
         val token = preferenceRepository.getDefaultAccountToken() ?: return SetupTokenOutputData(
