a65db4b83bb95d00a80e29c781118c17e15e213d
sunya9 <mugen.xyz@gmail.com>
2019-06-19 05:04:49 +0900

    Remove execute method

--- app/src/main/java/net/unsweets/gamma/domain/usecases/AsyncUseCase.kt
+++ app/src/main/java/net/unsweets/gamma/domain/usecases/AsyncUseCase.kt
@@ -1,13 +1,7 @@
 package net.unsweets.gamma.domain.usecases
 
-import kotlinx.coroutines.*
-
 abstract class AsyncUseCase<out Type, in Params> where Type : Any {
 
     abstract suspend fun run(params: Params): Type
 
-    fun execute(onResult: (Type) -> Unit, params: Params) {
-        val job = GlobalScope.async(Dispatchers.Default, CoroutineStart.DEFAULT) { run(params) }
-        GlobalScope.launch(Dispatchers.Main, CoroutineStart.DEFAULT) { onResult.invoke(job.await()) }
-    }
 }
