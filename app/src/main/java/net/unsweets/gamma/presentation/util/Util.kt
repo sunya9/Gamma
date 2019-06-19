14dd5bc4cdc397d920e87e942f21eef57c5ffead
sunya9 <mugen.xyz@gmail.com>
2019-06-20 02:57:23 +0900

    Add getViewPositionOnScreen method

--- app/src/main/java/net/unsweets/gamma/presentation/util/Util.kt
+++ app/src/main/java/net/unsweets/gamma/presentation/util/Util.kt
@@ -3,7 +3,6 @@ package net.unsweets.gamma.presentation.util
 import android.content.Context
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import androidx.annotation.IdRes
 
 fun showKeyboard(view: View) {
     val imm = getImm(view.context)
@@ -24,4 +23,12 @@ private fun getImm(context: Context) = context.getSystemService(Context.INPUT_ME
 
 interface DrawerContentFragment {
     val menuItemId: Int
+}
+
+fun getViewPositionOnScreen(view: View): Pair<Int, Int> {
+    val pos = IntArray(2)
+    view.getLocationOnScreen(pos)
+    val cx = pos[0] + view.width / 2
+    val cy = pos[1] + view.height / 2
+    return Pair(cx, cy)
 }
