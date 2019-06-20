a0b7f570116cbe82b962bb9517c14bc2de050250
sunya9 <mugen.xyz@gmail.com>
2019-06-20 09:25:17 +0900

    Set include_post_raw to 1

--- app/src/main/java/net/unsweets/gamma/domain/model/params/single/GeneralPostParam.kt
+++ app/src/main/java/net/unsweets/gamma/domain/model/params/single/GeneralPostParam.kt
@@ -3,10 +3,12 @@ package net.unsweets.gamma.domain.model.params.single
 import net.unsweets.gamma.util.toInt
 
 data class GeneralPostParam(
-    val includeDeleted: Boolean = false
+    val includeDeleted: Boolean = false,
+    val includePostRaw: Boolean = true
 ): BaseParam {
     override fun toMap(): Map<String, String> = hashMapOf<String, String>().also { map ->
         map["include_deleted"] = includeDeleted.toInt().toString()
+        map["include_post_raw"] = includePostRaw.toInt().toString()
     }
 
 }
