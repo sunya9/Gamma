4e89d9f771b5f02c4037c4b987fa0c5a9222429e
sunya9 <mugen.xyz@gmail.com>
2019-07-21 05:49:22 +0900

    Remove unused parameter class

--- app/src/main/java/net/unsweets/gamma/domain/model/params/composed/SearchParam.kt
+++
@@ -1,9 +0,0 @@
-package net.unsweets.gamma.domain.model.params.composed
-
-import net.unsweets.gamma.domain.model.params.composed.BaseComposeParam
-
-data class SearchParam(
-    val q: String
-): BaseComposeParam() {
-
-}
