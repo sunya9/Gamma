1e2134f3790842635542fc35f9dad8ab1e9b1d70
sunya9 <mugen.xyz@gmail.com>
2019-07-21 05:48:49 +0900

    Fix base class

--- app/src/main/java/net/unsweets/gamma/domain/model/params/composed/GetInteractionsParam.kt
+++ app/src/main/java/net/unsweets/gamma/domain/model/params/composed/GetInteractionsParam.kt
@@ -1,10 +1,13 @@
 package net.unsweets.gamma.domain.model.params.composed
 
 import net.unsweets.gamma.domain.model.params.single.InteractionParam
+import net.unsweets.gamma.domain.model.params.single.PaginationParam
 
-class GetInteractionsParam: GetPostsParam() {
+class GetInteractionsParam : BaseComposeParam() {
     init {
         add(InteractionParam())
     }
+
+    fun add(pagination: PaginationParam) = queryList.add(pagination)
     fun add(interactionParam: InteractionParam) = queryList.add(interactionParam)
 }
