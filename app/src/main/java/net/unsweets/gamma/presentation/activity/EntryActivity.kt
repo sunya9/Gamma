fd0b71bc9a152fe4fbc8e3ba568f606ac3e24167
sunya9 <mugen.xyz@gmail.com>
2019-07-25 08:48:01 +0900

    Fix clear cache timing

--- app/src/main/java/net/unsweets/gamma/presentation/activity/EntryActivity.kt
+++ app/src/main/java/net/unsweets/gamma/presentation/activity/EntryActivity.kt
@@ -2,13 +2,12 @@ package net.unsweets.gamma.presentation.activity
 
 import android.app.Activity
 import android.content.Intent
-import android.os.Bundle
 import android.view.View
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.runBlocking
-import net.unsweets.gamma.GammaApplication
 import net.unsweets.gamma.domain.usecases.SetupTokenUseCase
+import net.unsweets.gamma.service.ClearCacheService
 import javax.inject.Inject
 import kotlin.reflect.KClass
 
@@ -17,10 +16,9 @@ class EntryActivity : BaseActivity(), CoroutineScope by MainScope() {
     @Inject
     lateinit var setupTokenUseCase: SetupTokenUseCase
 
-    public override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-
+    override fun onResume() {
+        super.onResume()
+        ClearCacheService.startService(this)
         findViewById<View>(android.R.id.content).systemUiVisibility =
             View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
 
