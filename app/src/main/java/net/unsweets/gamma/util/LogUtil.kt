7ec5a49e0fdfe793e5edc378879bc69e26118c75
sunya9 <mugen.xyz@gmail.com>
2019-07-25 04:59:22 +0900

    Use object instead of class

--- app/src/main/java/net/unsweets/gamma/util/LogUtil.kt
+++ app/src/main/java/net/unsweets/gamma/util/LogUtil.kt
@@ -4,23 +4,25 @@ import android.util.Log
 import net.unsweets.gamma.BuildConfig
 
 
-class LogUtil {
-    companion object {
-        private val TAG: String? = LogUtil::class.java.canonicalName
-        fun e(message: String? = "") {
-            if(BuildConfig.DEBUG) Log.e(TAG, message)
-        }
-        fun d(message: String? = "") {
-            if(BuildConfig.DEBUG) Log.d(TAG, message)
-        }
-        fun i(message: String? = "") {
-            if(BuildConfig.DEBUG) Log.i(TAG, message)
-        }
-        fun w(message: String? = "") {
-            if(BuildConfig.DEBUG) Log.w(TAG, message)
-        }
-        fun v(message: String? = "") {
-            if(BuildConfig.DEBUG) Log.v(TAG, message)
-        }
+object LogUtil {
+    private val TAG: String? = LogUtil::class.java.canonicalName
+    fun e(message: String? = "") {
+        if (BuildConfig.DEBUG) Log.e(TAG, message)
+    }
+
+    fun d(message: String? = "") {
+        if (BuildConfig.DEBUG) Log.d(TAG, message)
+    }
+
+    fun i(message: String? = "") {
+        if (BuildConfig.DEBUG) Log.i(TAG, message)
+    }
+
+    fun w(message: String? = "") {
+        if (BuildConfig.DEBUG) Log.w(TAG, message)
+    }
+
+    fun v(message: String? = "") {
+        if (BuildConfig.DEBUG) Log.v(TAG, message)
     }
 }
