699382e4e5c27998aa4b90b40966330e17844a1b
sunya9 <mugen.xyz@gmail.com>
2019-09-02 23:39:24 +0900

    Fix ShareActivity theme

--- app/src/main/java/net/unsweets/gamma/presentation/activity/ShareActivity.kt
+++ app/src/main/java/net/unsweets/gamma/presentation/activity/ShareActivity.kt
@@ -1,15 +1,13 @@
 package net.unsweets.gamma.presentation.activity
 
 import android.content.Intent
-import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
-import android.view.View
-import android.view.WindowManager
 import dagger.android.support.DaggerAppCompatActivity
 import net.unsweets.gamma.domain.model.UriInfo
 import net.unsweets.gamma.presentation.fragment.ComposePostDialogFragment
 import net.unsweets.gamma.presentation.fragment.ComposePostFragment
+import net.unsweets.gamma.presentation.util.ThemeColorUtil
 
 class ShareActivity : DaggerAppCompatActivity(), ComposePostDialogFragment.Callback {
     override fun onDismiss() {
@@ -42,14 +40,9 @@ class ShareActivity : DaggerAppCompatActivity(), ComposePostDialogFragment.Callb
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        ThemeColorUtil.currentDarkThemeMode(this)
+        ThemeColorUtil.applyTheme(this)
         super.onCreate(savedInstanceState)
-        window.apply {
-            clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
-            addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-            decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-            statusBarColor = Color.TRANSPARENT
-
-        }
         val dm = resources.displayMetrics
         val cx = dm.widthPixels / 2
         val cy = dm.heightPixels / 2
