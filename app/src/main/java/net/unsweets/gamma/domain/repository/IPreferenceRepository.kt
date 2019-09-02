bb09169659af111a203f779742994917669e927d
sunya9 <mugen.xyz@gmail.com>
2019-09-03 01:47:17 +0900

    Remove unused methods

--- app/src/main/java/net/unsweets/gamma/domain/repository/IPreferenceRepository.kt
+++ app/src/main/java/net/unsweets/gamma/domain/repository/IPreferenceRepository.kt
@@ -1,9 +1,3 @@
 package net.unsweets.gamma.domain.repository
 
-interface IPreferenceRepository {
-    fun getDefaultAccountID(): String?
-    fun updateDefaultAccountID(id: String): Boolean
-    fun removeDefaultAccountIDAndToken(): Boolean
-    fun setAccountListOrder(ids: List<String>): Boolean
-    fun getAccountIdList(): List<String>?
-}
+interface IPreferenceRepository
--- app/src/main/java/net/unsweets/gamma/domain/repository/PreferenceRepository.kt
+++ app/src/main/java/net/unsweets/gamma/domain/repository/PreferenceRepository.kt
@@ -3,34 +3,12 @@ package net.unsweets.gamma.domain.repository
 import android.content.Context
 
 class PreferenceRepository(val context: Context) : AbstractPreferenceRepository(context), IPreferenceRepository {
-    override fun getAccountIdList(): List<String>? {
-        return sharedPreferences.getString(PrefKey.AccountListOrder.key, null)?.split(",")
-    }
-
-    override fun setAccountListOrder(ids: List<String>): Boolean {
-        return editor.putString(PrefKey.AccountListOrder.key, ids.joinToString(",")).commit()
-
-    }
 
     override val name = "Store"
     enum class PrefKey(val key: String) {
         ID("id"),
         Token("token"),
         AccountListOrder("account_list_order")
-    }
-
-    override fun getDefaultAccountID(): String? = sharedPreferences.getString(PrefKey.ID.key, null)
-
-    override fun updateDefaultAccountID(id: String): Boolean {
-        return editor.putString(PrefKey.ID.name, id).commit()
-    }
-
-    override fun removeDefaultAccountIDAndToken(): Boolean {
-        return editor.run {
-            remove(PrefKey.ID.key)
-            remove(PrefKey.Token.key)
-            commit()
-        }
     }
 
 
