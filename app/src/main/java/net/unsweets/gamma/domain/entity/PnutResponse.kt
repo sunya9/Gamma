852b8624cfe5355a2f9f5d934335532cb02498f4
sunya9 <mugen.xyz@gmail.com>
2019-09-06 07:30:21 +0900

    Set default value

--- app/src/main/java/net/unsweets/gamma/domain/entity/PnutResponse.kt
+++ app/src/main/java/net/unsweets/gamma/domain/entity/PnutResponse.kt
@@ -3,8 +3,8 @@ package net.unsweets.gamma.domain.entity
 data class PnutResponse<T>(val meta: Meta, val data: T) {
     data class Meta(
         val code: Int,
-        val min_id: String?,
-        val max_id: String?,
-        val more: Boolean?
+        val min_id: String? = null,
+        val max_id: String? = null,
+        val more: Boolean? = null
     )
 }
