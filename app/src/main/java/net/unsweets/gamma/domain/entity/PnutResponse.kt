7fb373bbfe7558d90ad64a46a198237d4392e03b
sunya9 <mugen.xyz@gmail.com>
2019-09-07 06:20:22 +0900

    Add deletedIds and revisedIds fields

--- app/src/main/java/net/unsweets/gamma/domain/entity/PnutResponse.kt
+++ app/src/main/java/net/unsweets/gamma/domain/entity/PnutResponse.kt
@@ -1,10 +1,14 @@
 package net.unsweets.gamma.domain.entity
 
+import com.squareup.moshi.Json
+
 data class PnutResponse<T>(val meta: Meta, val data: T) {
     data class Meta(
         val code: Int,
         val min_id: String? = null,
         val max_id: String? = null,
-        val more: Boolean? = null
+        val more: Boolean? = null,
+        @Json(name = "deleted_ids") val deletedIds: List<String>? = null,
+        @Json(name = "revised_ids") val revisedIds: List<String>? = null
     )
 }
