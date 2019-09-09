ad50ad2cc4d01201bdaceaf1cdb0b4ae77357153
sunya9 <mugen.xyz@gmail.com>
2019-09-09 13:36:30 +0900

    Add initialized argument

--- app/src/main/java/net/unsweets/gamma/presentation/util/InfiniteScrollListener.kt
+++ app/src/main/java/net/unsweets/gamma/presentation/util/InfiniteScrollListener.kt
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.RecyclerView
 class InfiniteScrollListener(val listener: Callback) : RecyclerView.OnScrollListener() {
     interface Callback {
         fun onLoadMore()
+        val initialized: Boolean
     }
     override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
         super.onScrolled(recyclerView, dx, dy)
@@ -13,7 +14,7 @@ class InfiniteScrollListener(val listener: Callback) : RecyclerView.OnScrollList
         val totalItemCount = linearLayoutManager.itemCount
         val lastVisibleItem = linearLayoutManager.findLastVisibleItemPosition()
         val visibleThreshold = 20
-        if (totalItemCount <= (lastVisibleItem + visibleThreshold)) {
+        if (totalItemCount <= (lastVisibleItem + visibleThreshold) && listener.initialized) {
             listener.onLoadMore()
         }
     }
