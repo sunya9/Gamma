a7d12529146dcceb13960433941392aa6d2e55e5
sunya9 <mugen.xyz@gmail.com>
2019-09-12 09:52:26 +0900

    Detect preferences changes

--- app/src/main/java/net/unsweets/gamma/presentation/activity/BaseActivity.kt
+++ app/src/main/java/net/unsweets/gamma/presentation/activity/BaseActivity.kt
@@ -1,10 +1,13 @@
 package net.unsweets.gamma.presentation.activity
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.MenuItem
 import dagger.android.support.DaggerAppCompatActivity
 import net.unsweets.gamma.GammaApplication
+import net.unsweets.gamma.domain.repository.IPreferenceRepository
 import net.unsweets.gamma.presentation.util.ThemeColorUtil
+import javax.inject.Inject
 
 abstract class BaseActivity : DaggerAppCompatActivity() {
     private lateinit var darkThemeMode: String
@@ -12,11 +15,21 @@ abstract class BaseActivity : DaggerAppCompatActivity() {
 
     private enum class StateKey { ConfigurationChanges }
 
+    @Inject
+    lateinit var preferenceRepository: IPreferenceRepository
+
+    private var preferencesAreChanged = false
+    private val changePreferenceListener =
+        SharedPreferences.OnSharedPreferenceChangeListener { _, _ ->
+            preferencesAreChanged = true
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         GammaApplication.getInstance(this).updateBaseTheme()
         darkThemeMode = ThemeColorUtil.currentDarkThemeMode(this)
         themeColorWhenCreated = ThemeColorUtil.applyTheme(this)
         super.onCreate(savedInstanceState)
+        preferenceRepository.onRegisterChangePreference(changePreferenceListener)
     }
 
 
@@ -29,7 +42,7 @@ abstract class BaseActivity : DaggerAppCompatActivity() {
         super.onResume()
         val updateThemeColor = themeColorWhenCreated != ThemeColorUtil.getThemeColor(this)
         val currentDarkThemeMode = ThemeColorUtil.currentDarkThemeMode(this)
-        if (updateThemeColor || darkThemeMode != currentDarkThemeMode) {
+        if (updateThemeColor || darkThemeMode != currentDarkThemeMode || preferencesAreChanged) {
             recreate()
         }
     }
