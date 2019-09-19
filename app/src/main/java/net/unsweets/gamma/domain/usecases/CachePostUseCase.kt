28831188f2e63f01aca1726ed6b01a7773524267
sunya9 <mugen.xyz@gmail.com>
2019-09-20 07:38:48 +0900

    Disable cache in missed conversations

--- app/src/main/java/net/unsweets/gamma/domain/usecases/CachePostUseCase.kt
+++ app/src/main/java/net/unsweets/gamma/domain/usecases/CachePostUseCase.kt
@@ -1,5 +1,6 @@
 package net.unsweets.gamma.domain.usecases
 
+import net.unsweets.gamma.domain.model.StreamType
 import net.unsweets.gamma.domain.model.io.CachePostInputData
 import net.unsweets.gamma.domain.repository.IPnutCacheRepository
 import net.unsweets.gamma.domain.repository.IPreferenceRepository
@@ -12,6 +13,7 @@ class CachePostUseCase(
     AsyncUseCase<Unit, CachePostInputData>() {
     override suspend fun run(params: CachePostInputData) {
         LogUtil.e("CachePostUseCase")
+        if (params.streamType == StreamType.Explore.MissedConversations) return
         pnutCacheRepository.storePosts(
             params.list,
             params.streamType,
--- app/src/main/java/net/unsweets/gamma/domain/usecases/GetCachedPostListUseCase.kt
+++ app/src/main/java/net/unsweets/gamma/domain/usecases/GetCachedPostListUseCase.kt
@@ -1,5 +1,7 @@
 package net.unsweets.gamma.domain.usecases
 
+import net.unsweets.gamma.domain.model.CachedList
+import net.unsweets.gamma.domain.model.StreamType
 import net.unsweets.gamma.domain.model.io.GetCachedPostListInputData
 import net.unsweets.gamma.domain.model.io.GetCachedPostListOutputData
 import net.unsweets.gamma.domain.repository.IPnutCacheRepository
@@ -7,6 +9,9 @@ import net.unsweets.gamma.domain.repository.IPnutCacheRepository
 class GetCachedPostListUseCase(private val pnutCacheRepository: IPnutCacheRepository) :
     AsyncUseCase<GetCachedPostListOutputData, GetCachedPostListInputData>() {
     override suspend fun run(params: GetCachedPostListInputData): GetCachedPostListOutputData {
+        if (params.streamType == StreamType.Explore.MissedConversations) return GetCachedPostListOutputData(
+            CachedList(emptyList())
+        )
         val posts = pnutCacheRepository.getPosts(params.streamType)
         return GetCachedPostListOutputData(posts)
     }
