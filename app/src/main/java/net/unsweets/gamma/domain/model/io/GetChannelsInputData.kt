9ca8eedc3336f421f6d48f798f7a4c1e42d3b554
sunya9 <mugen.xyz@gmail.com>
2020-02-19 01:13:13 +0900

    Move ChannelType

--- app/src/main/java/net/unsweets/gamma/domain/model/io/GetChannelsInputData.kt
+++ app/src/main/java/net/unsweets/gamma/domain/model/io/GetChannelsInputData.kt
@@ -1,10 +1,9 @@
 package net.unsweets.gamma.domain.model.io
 
+import net.unsweets.gamma.domain.model.ChannelType
 import net.unsweets.gamma.domain.model.params.composed.GetChannelsParam
 
 data class GetChannelsInputData(
     val channelType: ChannelType,
     val params: GetChannelsParam
-) {
-    enum class ChannelType
-}
+)
