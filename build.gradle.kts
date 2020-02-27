fd100220effc4843bd340dd1af6c44daa18f3d8d
sunya9 <mugen.xyz@gmail.com>
2020-02-28 05:38:40 +0900

    Upgrade dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,10 +8,10 @@ buildscript {
         maven(url = "https://maven.fabric.io/public")
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.5.3")
+      classpath("com.android.tools.build:gradle:3.6.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         classpath("com.google.gms:oss-licenses:0.9.2")
-        classpath("com.google.gms:google-services:4.3.2")
+      classpath("com.google.gms:google-services:4.3.3")
         classpath("io.fabric.tools:gradle:1.31.1")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
