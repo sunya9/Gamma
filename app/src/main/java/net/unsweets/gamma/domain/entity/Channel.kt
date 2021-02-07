9a07fbe44beb0b523561ca84ab7d928567230ce9
sunya9 <mugen.xyz@gmail.com>
2021-02-08 03:47:55 +0900

    feat: add LimitedUser class

--- app/src/main/java/net/unsweets/gamma/domain/entity/Channel.kt
+++ app/src/main/java/net/unsweets/gamma/domain/entity/Channel.kt
@@ -5,16 +5,17 @@ import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 import kotlinx.android.parcel.IgnoredOnParcel
 import kotlinx.android.parcel.Parcelize
+import net.unsweets.gamma.domain.entity.raw.ChatSettings
 
 @Parcelize
 @JsonClass(generateAdapter = true)
 data class Channel(
     val id: String,
-    @Json(name = "is_active") val isActive: Boolean,
+    @Json(name = "is_active") val isActive: Boolean? = null,
     val type: String,
     val owner: User,
-    @Json(name = "recent_message_id") val recentMessageId: String,
-    @Json(name = "recent_message") val recentMessage: Message,
+    @Json(name = "recent_message_id") val recentMessageId: String? = null,
+    @Json(name = "recent_message") val recentMessage: Message? = null,
     val acl: Acl,
     val counts: ChannelCount,
     @Json(name = "you_subscribed") val youSubscribed: Boolean,
@@ -43,7 +44,7 @@ data class Channel(
         data class Full(
             override val immutable: Boolean,
             override val you: Boolean,
-            @Json(name = "user_ids") override val userIds: List<String>
+            @Json(name = "user_ids") override val userIds: List<LimitedUser>
         ) : IAuthority, Parcelable
 
         @Parcelize
@@ -51,7 +52,7 @@ data class Channel(
         data class Write(
             override val immutable: Boolean,
             override val you: Boolean,
-            @Json(name = "user_ids") override val userIds: List<String>,
+            @Json(name = "user_ids") override val userIds: List<LimitedUser>,
             @Json(name = "any_user") val anyUser: Boolean
         ) : IAuthority, Parcelable
 
@@ -60,7 +61,7 @@ data class Channel(
         data class Read(
             override val immutable: Boolean,
             override val you: Boolean,
-            @Json(name = "user_ids") override val userIds: List<String>,
+            @Json(name = "user_ids") override val userIds: List<LimitedUser>,
             @Json(name = "any_user") val anyUser: Boolean,
             val public: Boolean
         ) : IAuthority, Parcelable
@@ -69,9 +70,27 @@ data class Channel(
             val immutable: Boolean
             val you: Boolean
             @Json(name = "user_ids")
-            val userIds: List<String>
+            val userIds: List<LimitedUser>
         }
     }
 
+    @Parcelize
+    @JsonClass(generateAdapter = true)
+    data class LimitedUser(
+        val username: String,
+        val id: String,
+        val name: String?,
+        @Json(name ="avatar_image") val avatarImage: String,
+        val presence: Boolean
+    )
 
+    val isPrivate = type === privateChannelType
+    val isPublic = type === publicChannelType
+    val chatSettings: ChatSettings? = null
+
+
+    companion object {
+        const val privateChannelType = "io.pnut.core.pm"
+        const val publicChannelType = "io.pnut.core.chat"
+    }
 }
