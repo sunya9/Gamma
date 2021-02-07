68c116a0e8e00eb81b26a5662e3f4f13bc0bc575
sunya <mugen.xyz@gmail.com>
2021-02-08 00:41:29 +0900

    Upgrade modules (#41)
    * feat: upgrade modules
    
    * fix: dataBinding declaration
    
    * fix: increment sdkVersion

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-    val kotlinVersion: String by extra { "1.3.60" }
+    val kotlinVersion: String by extra { "1.4.30" }
     repositories {
         google()
         jcenter()
@@ -11,11 +11,11 @@ buildscript {
 
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:4.1.1")
+        classpath("com.android.tools.build:gradle:4.1.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         classpath("com.google.gms:oss-licenses:0.9.2")
-        classpath("com.google.gms:google-services:4.3.4")
-        classpath("com.google.firebase:firebase-crashlytics-gradle:2.3.0")
+        classpath("com.google.gms:google-services:4.3.5")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:2.4.1")
         val navVersion = "2.3.1"
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$navVersion")
         // NOTE: Do not place your application dependencies here; they belong
@@ -24,7 +24,7 @@ buildscript {
 }
 
 allprojects {
-    val kotlinVersion: String by extra { "1.3.50" }
+    val kotlinVersion: String by extra { "1.4.30" }
     repositories {
         google()
         jcenter()
