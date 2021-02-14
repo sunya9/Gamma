299ba04c1b6137a3ab016212bd2970efff95ef7e
sunya9 <mugen.xyz@gmail.com>
2021-02-15 02:42:47 +0900

    fix: avoid null warning

--- app/src/main/java/net/unsweets/gamma/util/LogUtil.kt
+++ app/src/main/java/net/unsweets/gamma/util/LogUtil.kt
@@ -7,22 +7,27 @@ import net.unsweets.gamma.BuildConfig
 object LogUtil {
     private val TAG: String? = LogUtil::class.java.canonicalName
     fun e(message: String? = "") {
-        if (BuildConfig.DEBUG) Log.e(TAG, message)
+        if (!BuildConfig.DEBUG) return
+        message?.let { Log.e(TAG, it) }
     }
 
     fun d(message: String? = "") {
-        if (BuildConfig.DEBUG) Log.d(TAG, message)
+        if (!BuildConfig.DEBUG) return
+        message?.let { Log.d(TAG, it) }
     }
 
     fun i(message: String? = "") {
-        if (BuildConfig.DEBUG) Log.i(TAG, message)
+        if (!BuildConfig.DEBUG) return
+        message?.let { Log.i(TAG, it) }
     }
 
     fun w(message: String? = "") {
-        if (BuildConfig.DEBUG) Log.w(TAG, message)
+        if (!BuildConfig.DEBUG) return
+        message?.let { Log.w(TAG, it) }
     }
 
     fun v(message: String? = "") {
-        if (BuildConfig.DEBUG) Log.v(TAG, message)
+        if (!BuildConfig.DEBUG) return
+        message?.let { Log.v(TAG, it) }
     }
 }
