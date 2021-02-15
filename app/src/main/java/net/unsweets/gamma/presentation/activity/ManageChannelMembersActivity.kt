e8598799719b8771ad06037aa2fca75a22944092
sunya9 <mugen.xyz@gmail.com>
2021-02-15 22:19:33 +0900

    feat: add ManageChannelMembersActivity

---
+++ app/src/main/java/net/unsweets/gamma/presentation/activity/ManageChannelMembersActivity.kt
@@ -0,0 +1,12 @@
+package net.unsweets.gamma.presentation.activity
+
+import android.os.Bundle
+import net.unsweets.gamma.R
+
+class ManageChannelMembersActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_manage_channel_members)
+    }
+}
