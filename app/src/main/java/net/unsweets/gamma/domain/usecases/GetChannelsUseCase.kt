d9b954df1e69784fd5080ee4d85bcfa9edfe8956
sunya9 <mugen.xyz@gmail.com>
2021-02-19 03:28:34 +0900

    fix: set always includeLimitedUsers as true

--- app/src/main/java/net/unsweets/gamma/domain/usecases/GetChannelsUseCase.kt
+++ app/src/main/java/net/unsweets/gamma/domain/usecases/GetChannelsUseCase.kt
@@ -2,12 +2,13 @@ package net.unsweets.gamma.domain.usecases
 
 import net.unsweets.gamma.domain.model.io.GetChannelsInputData
 import net.unsweets.gamma.domain.model.io.GetChannelsOutputData
+import net.unsweets.gamma.domain.model.params.single.GeneralChannelParam
 import net.unsweets.gamma.domain.repository.IPnutRepository
 
 class GetChannelsUseCase(private val pnutRepository: IPnutRepository) :
     AsyncUseCase<GetChannelsOutputData, GetChannelsInputData>() {
     override suspend fun run(params: GetChannelsInputData): GetChannelsOutputData {
-        val channels = pnutRepository.getChannels(params.params)
+        val channels = pnutRepository.getChannels(params.params.add(GeneralChannelParam(includeLimitedUsers = true)))
         return GetChannelsOutputData(channels)
     }
 }
